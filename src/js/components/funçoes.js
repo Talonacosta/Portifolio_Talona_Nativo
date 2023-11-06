378181ab93d5ab2e68040dae76903b2e0e799808
Talonacosta <talonacontato@gmail.com>
2023-11-06 19:47:18 -0300

    bu

--- src/js/components/funçoes.js
+++ src/js/components/funçoes.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+ HEAD
 function copyToClipboard(text) {
     var tempInput = document.createElement('input');
     tempInput.value = text;
@@ -14,20 +14,3 @@ function copyToClipboard(text) {
         discordLink.textContent = 'Discord';
     }, 3000);
 }
-=======
-function copyToClipboard(text) {
-    var tempInput = document.createElement('input');
-    tempInput.value = text;
-    document.body.appendChild(tempInput);
-    tempInput.select();
-    document.execCommand('copy');
-    document.body.removeChild(tempInput);
-
-    var discordLink = document.getElementById('discord-link');
-    discordLink.textContent = 'Copiado';
-
-    setTimeout(function () {
-        discordLink.textContent = 'Discord';
-    }, 3000);
-}
->>>>>>> 136b70839c21b6203dd24554f0f6990a524d24e8
